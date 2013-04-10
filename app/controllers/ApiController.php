ea6d624216d6851ee25e744a7f20f1f7073fe311
jaequery <jaequery@gmail.com>
2013-04-09 22:36:37 -0700

    adding login

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -2,14 +2,13 @@
 
 class ApiController extends BaseController {
 
-    private function respond($params, $code = 200, $msg = 'ok'){
-        $res = array(
+    private function respond($res = array(), $code = 200, $msg = 'ok'){
+        $res = array_merge($res, array(
             "status" => array(
                 "code" => $code,
                 "msg" => $msg,
             ),
-        );
-        $res = array_merge($params, $res);
+        ));
         return Response::json($res);
     }
 
@@ -20,14 +19,14 @@ class ApiController extends BaseController {
 
     public function auth()
     {
-        $res = array();
         $credentials = array('username' => Input::get('username'), 'password' => Input::get('password'));
-        if (Auth::attempt($credentials)){
-            if(!Request::ajax()) return Redirect::to('/admin');
-            $res = array($credentials);
+        $auth = Auth::attempt($credentials);
+        $res = array('auth' => $auth);
+        if ($auth){
+            //if(!Request::ajax()) return Redirect::to('/admin');
             return $this->respond($res, 200, 'logged in');
         }else{
-            if(!Request::ajax()) return Redirect::to('/admin');
+            //if(!Request::ajax()) return Redirect::to('/admin');
             return $this->respond($res, 404, 'user not found');
         }
         
