7f5589db15120c41d74b57c46e7c9a052cfb92fe
jaequery <jaequery@gmail.com>
2013-04-09 23:35:31 -0700

    updated test pw

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'bootphp',
 			'username'  => 'root',
-			'password'  => 'test',
+			'password'  => 'j',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
