95aae7e81bd6fc53df5de0ab173209417a58ee8d
jaequery <jaequery@gmail.com>
2013-04-09 23:22:11 -0700

    ignoring database config

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'bootphp',
 			'username'  => 'root',
-			'password'  => 'j',
+			'password'  => 'test',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
